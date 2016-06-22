8cc4d7324df1d988b0aecbd5fbcf6c9d4792c896
a-pinch <shmat.co@mail.ru>
2016-06-22 12:06:50 +0300

    distinct reduser

--- 41_9/distinctMapper.py
+++ 41_9/distinctMapper.py
@@ -1,18 +1,7 @@
 #! /usr/bin/python3
 import sys
 
-oldKey = ''
-sum=cnt=0
 for line in sys.stdin:
     l = line.strip().split("\t")
-    value = l[1].strip().split(";")
-    if l[0] == oldKey:
-        sum = sum + int(value[0])
-        cnt = cnt + 1
-    else:
-        if oldKey: print(oldKey+"\t"+str(sum)+";"+str(cnt))
-        oldKey = l[0]
-        sum = int(value[0])
-        cnt = 1
-if oldKey: print(oldKey+"\t"+str(sum)+";"+str(cnt))
-       
+    for g in l[1].strip().split(","):
+        print(l[0]+","+g+"\t1"); 
