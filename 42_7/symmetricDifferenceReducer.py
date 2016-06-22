9f08b43b727b4655e6fb54fb1424c760169770cb
a-pinch <shmat.co@mail.ru>
2016-06-22 22:06:05 +0300

    symmetric difference reducer

---
+++ 42_7/symmetricDifferenceReducer.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+import sys
+
+oldKey = ''
+oldValue = ''
+n = 0
+for line in sys.stdin:
+    l = line.strip().split()
+    if(l[0] == oldKey):
+        n += 1
+    else:
+        if(n == 1 ):
+            print(oldKey)            
+        n = 1
+    oldKey = l[0]
+    oldValue = l[1]
+if(n == 1):
+    print(oldKey)   
