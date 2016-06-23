935c4514bfcd3722330d1b59150e78e4c218450e
a-pinch <shmat.co@mail.ru>
2016-06-23 14:15:23 +0300

    tf-ids mapper 1

--- 43_2/tfidfMapper1.py
+++ 43_2/tfidfMapper1.py
@@ -1,23 +1,8 @@
 #! /usr/bin/python3
 import sys
 
-oldKey = ''
-H = {}
 for line in sys.stdin:
-    l = line.strip().split("   ") 
-    m = l[1].strip().split(":")
-    if(l[0] != oldKey):
-        if('query' in H):
-            for q in H['query']:
-                if('url' in H):
-                    for u in H['url']:
-                        print(oldKey + "\t" + q + "\t" + u)
-        H = {}
-        oldKey = l[0]
-    if(m[0] in H): H[m[0]].append(m[1])
-    else: H[m[0]] = [m[1]]
-if('query' in H):        
-    for q in H['query']:
-        if('url' in H):
-            for u in H['url']:
-                print(oldKey + "\t" + q + "\t" + u)
+    l = line.strip().split(":") 
+    for w in l[1].strip().split(" "):
+        print(w+"#"+l[0]+"\t1")
+    
