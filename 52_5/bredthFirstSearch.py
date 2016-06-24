2648c2795a91b77eb0611dafbb627845302fbb86
a-pinch <shmat.co@mail.ru>
2016-06-24 12:51:50 +0300

    Breadth First Search Mapper

--- 52_5/bredthFirstSearch.py
+++ 52_5/bredthFirstSearch.py
@@ -1,43 +1,9 @@
 #! /usr/bin/python3
-import sys
+import sys,re
 
-def getMinEdge(q, d, w):
-    "find short way to q edge"
-    m = -1
-    for c,k in d.items():
-        if((c,q) in w):
-            if(m < 0):
-                m = k+w[(c,q)]
-            else:
-                if(k+w[(c,q)]<m):
-                    m = k+w[(c,q)]
-    return m
-
-v = r = s = e = i = 0
-w = {}
-d = {}
-
+r = re.compile("\W+")
 for line in sys.stdin:    
+    print(line,end="")
     l = line.strip().split() 
-    if(i == 0):
-        v = int(l[0])
-        r = int(l[1])
-    else:
-        if(i <= r):
-            w[(int(l[0]),int(l[1]))] = int(l[2])
-        else:
-            s = int(l[0])
-            e = int(l[1])
-            d[s] = 0
-    i+=1
-while e not in d:
-    mm = -1
-    mi = e
-    for i in range(v):
-        if(i+1 not in d):
-            m = getMinEdge(i+1, d, w)
-            if(m>-1 and (m<mm or mm<0)):
-                mm = m
-                mi = i+1
-    d[mi] = mm
-print(d[e])
+    for e in r.split(l[2]):
+        if(e): print(e+"\t"+(str(int(l[1])+1) if isinstance(l[1],int) else l[1])+"\t{}")
