344b090565e2315ccd80c0189f3b33de582a6199
a-pinch <shmat.co@mail.ru>
2016-06-24 15:38:51 +0300

    page rank mapper

--- 53_8/pageRankMapper.py
+++ 53_8/pageRankMapper.py
@@ -5,5 +5,8 @@ r = re.compile("\W+")
 for line in sys.stdin:    
     l = line.strip().split() 
     print(l[0]+"\t"+l[1]+"\t"+l[2])
+    n = 0.0;
     for e in r.split(l[2]):
-        if(e): print(e+"\t"+(str(int(l[1])+1) if l[1].isdigit() else l[1])+"\t{}")
+        if(e): n+=1.0
+    for e in r.split(l[2]):        
+        if(e): print("%s\t%.3f\t{}" % (e, float(l[1])/n)) 
