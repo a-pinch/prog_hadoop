c794c9f54d3d8a178bafdb60781a0045abed35f4
a-pinch <shmat.co@mail.ru>
2016-06-24 11:47:28 +0300

    dijkstra

--- 52_3/dijkstra.py
+++ 52_3/dijkstra.py
@@ -1,9 +1,43 @@
 #! /usr/bin/python3
-import sys,re
+import sys
 
-for line in sys.stdin:
-    l = line.strip().split(":",1) 
-    r = re.compile("\W+")
-    for w in r.split(l[1]):
-        if(w):  print(w+"#"+l[0]+"\t1")
-    
+def getMinEdge(q, d, w):
+    "find short way to q edge"
+    m = -1
+    for c,k in d.items():
+        if(w[(c,q)]):
+            if(m < 0):
+                m = k+w[(c,q)]
+            else:
+                if(k+w[(c,q)]<m):
+                    m = k+w[(c,q)]
+    return m
+
+v = r = s = e = i = 0
+w = {}
+d = {}
+
+for line in sys.stdin:    
+    l = line.strip().split() 
+    if(i == 0):
+        v = int(l[0])
+        r = int(l[1])
+    else:
+        if(i <= r):
+            w[(int(l[0]),int(l[1]))] = int(l[2])
+        else:
+            s = int(l[0])
+            e = int(l[1])
+            d[s] = 0
+    i+=1
+while e not in d:
+    mm = -1
+    mi = e
+    for i in range(v):
+        if(i+1 not in d):
+            m = getMinEdge(i+1, d, w)
+            if(m>-1 and (m<mm or mm<0)):
+                mm = m
+                mi = i+1
+    d[mi] = mm
+print(d[e])
