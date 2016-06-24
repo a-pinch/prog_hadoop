22fbe78c38f316222c57415676c1d61cb44d9d57
a-pinch <shmat.co@mail.ru>
2016-06-24 13:08:14 +0300

    fix tab

--- 52_5/bredthFirstSearch.py
+++ 52_5/bredthFirstSearch.py
@@ -3,7 +3,7 @@ import sys,re
 
 r = re.compile("\W+")
 for line in sys.stdin:    
-    print(line,end="")
     l = line.strip().split() 
+    print(l[0]+"\t"+l[1]+"\t"+l[2])
     for e in r.split(l[2]):
         if(e): print(e+"\t"+(str(int(l[1])+1) if l[1].isdigit() else l[1])+"\t{}")
